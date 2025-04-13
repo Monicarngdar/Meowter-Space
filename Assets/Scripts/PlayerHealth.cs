3224f8c821f817e516e7bd8deefe545d8858c655
Monicarngdar <monica.arong.darmanin@gmail.com>
2025-04-13 11:27:16 +0200

    Player Health
    Player loses a heart when they hit an obstacle

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
@@ -8,25 +9,22 @@ public class PlayerHealth : MonoBehaviour
    public Sprite currentHeart;
    public Sprite damageHeart;
 
-   void Update()
-   {
-      foreach (Image heart in hearts)
-      {
-         heart.sprite = damageHeart;
-      }
-
-      for (int i = 0; i < health; i++)
-      {
-         hearts[i].sprite = currentHeart;
-      }
-   }
-   
-   public void TakeDamage(int amount)
+ public void TakeDamage(int amount)
    {
       health -= amount;
-      if (health < 0)
+      if (health < 0) health = 0;
+
+      UpadatedHearts();
+   }
+
+   private void UpadatedHearts()
+   {
+      for (int i = 0; i < hearts.Length; i++)
       {
-         health = 0;
+         if (i < health)
+            hearts[i].sprite = currentHeart;
+         else 
+            hearts[i].sprite = damageHeart;
       }
    }
 }
--- Assets/Scripts/PlayerObstacleDamage.cs
+++ Assets/Scripts/PlayerObstacleDamage.cs
@@ -15,4 +15,6 @@ public class PlayerObstacleDamage : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+  
 }
